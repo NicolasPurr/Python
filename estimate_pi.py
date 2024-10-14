9a428c89c1a106409af26e8206ed28c9800cac34
nicolasPurr <123591418+NicolasPurr@users.noreply.github.com>
2024-10-14 16:40:30 +0200

    Initial commit

---
+++ estimate_pi.py
@@ -0,0 +1,20 @@
+import math
+import random
+
+iterations = int(input("Podaj liczbę iteracji:"))
+step = int(input("Podaj krok:"))
+hits = 0
+
+for i in range(iterations):
+    x = random.uniform(0, 1)
+    y = random.uniform(0, 1)
+    pytagoras = math.sqrt(x**2 + y**2)
+    if pytagoras <= 1.0:
+        hits += 1
+    if i == step:
+        print(4.0 * hits / i)
+        step *= 2
+
+pi_estimate = 4.0 * hits / iterations
+print(pi_estimate)
+print(math.pi - pi_estimate)
